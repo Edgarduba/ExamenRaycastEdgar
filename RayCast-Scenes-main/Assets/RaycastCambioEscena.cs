a0755f615f3244da4e7c39c7efb781a7fcc75378
Edgarduba <edgarduranbadell@gmail.com>
2022-12-02 13:50:47 +0100

    final

---
+++ RayCast-Scenes-main/Assets/RaycastCambioEscena.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RaycastCambioEscena : MonoBehaviour
+{
+    [SerializeField]private LayerMask rayLayer;
+    [SerializeField]private LayerMask firstSceneLayer;
+    [SerializeField]private LayerMask secondSceneLayer;
+    [SerializeField]private LayerMask thirdSceneLayer;
+
+    [SerializeField]private bool cronometro = false;
+    void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 20f, rayLayer))
+        {
+            Vector3 hitPosition = hit.point;
+            float hitDistance = hit.distance;
+            string hitName = hit.transform.name;
+
+        }
+        if(Input.GetButtonDown("Fire1"))
+        {
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit2;
+            if(Physics.Raycast(ray,out hit2))
+            {
+                transform.position = new Vector3(hit2.point.x, transform.position.y, hit2.point.z);
+                if(hit2.transform.gameObject.layer == 6)
+                {
+                    cronometro = true;
+                    LoadScene(1);
+                    Debug.Log("Cubo1");
+                }
+                else if(hit2.transform.gameObject.layer == 7)
+                {
+                    cronometro = true;
+                    LoadScene(2);
+                    Debug.Log("Esfera");
+                }
+                else if(hit2.transform.gameObject.layer == 8)
+                {
+                    cronometro = true;
+                    LoadScene(3);
+                    Debug.Log("Cubo2");
+                }
+    
+            }
+        }
+    }  
+}
+
